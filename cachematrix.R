93c9a535f52385e42c290a4abee940f145dcdc72
itamaracampos <itamarassc@gmail.com>
2021-03-27 14:59:29 +0100

    All functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -32,4 +31,6 @@ cacheSolve <- function(x, ...) {
   inv <- solve(mat, ...)
   x$setInverse(inv)
   inv
-  }
+}
+
+
